c1a66b7fdb95dface32bd9a9d3ff17d7f353e2a7
vakser <65089446+vakser@users.noreply.github.com>
2023-09-30 10:42:05 +0300

    Wrongly placed code moved from ReturnBook to Carousel file.

--- frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
+++ frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
@@ -1,6 +1,59 @@
 import {ReturnBook} from "./ReturnBook";
+import React, {useEffect, useState} from "react";
+import BookModel from "../../../models/BookModel";
 
 export const Carousel = () => {
+    const [books, setBooks] = useState<BookModel[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const baseUrl: string = "http://localhost:8080/api/books";
+            const url: string = `${baseUrl}?page=0&size=9`;
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+            const responseJson = await response.json();
+            const responseData = responseJson._embedded.books;
+            const loadedBooks: BookModel[] = [];
+            for (const key in responseData) {
+                loadedBooks.push({
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    author: responseData[key].author,
+                    description: responseData[key].description,
+                    copies: responseData[key].copies,
+                    copiesAvailable: responseData[key].copiesAvailable,
+                    category: responseData[key].category,
+                    img: responseData[key].img,
+                });
+            }
+            setBooks(loadedBooks);
+            setIsLoading(false);
+        };
+        fetchBooks().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+    }, []);
+
+    if (isLoading) {
+        return (
+            <div className='container m-5'>
+                <p>Loading...</p>
+            </div>
+        )
+    }
+    if (httpError) {
+        return (
+            <div className='container m-5'>
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
     return (
         <div className='container mt-5' style={{height: 550}}>
             <div className='homepage-carousel-title'>
