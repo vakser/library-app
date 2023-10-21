bb04d16eb53166dfc5bbee495da3502dadbb05eb
vakser <65089446+vakser@users.noreply.github.com>
2023-10-21 11:39:58 +0300

    Book checkout page component created and connected with search book component of React module of the library application.

--- frontend/react-library/src/layouts/SearchBooksPage/components/SearchBook.tsx
+++ frontend/react-library/src/layouts/SearchBooksPage/components/SearchBook.tsx
@@ -1,5 +1,6 @@
 import BookModel from "../../../models/BookModel"
 import React from "react";
+import {Link} from "react-router-dom";
 
 export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
     return (
@@ -23,7 +24,7 @@ export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
                     </div>
                 </div>
                 <div className='col-md-4 d-flex justify-content-center align-items-center'>
-                    <a className='btn btn-md main-color text-white' href='#'>View Details</a>
+                    <Link className='btn btn-md main-color text-white' to={`/checkout/${props.book.id}`}>View Details</Link>
                 </div>
             </div>
         </div>
