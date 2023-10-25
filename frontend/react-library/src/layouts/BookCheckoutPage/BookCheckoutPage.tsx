fdc85748798a53374c302fae9855ff8fc78962ea
vakser <65089446+vakser@users.noreply.github.com>
2023-10-25 20:52:23 +0300

    Stars review and Checkout and review box components added to React module of the library application.

--- frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,6 +1,8 @@
 import {useEffect, useState} from "react";
 import BookModel from "../../models/BookModel";
 import {SpinnerLoading} from "../Utils/SpinnerLoading";
+import {StarsReview} from "../Utils/StarsReview";
+import {CheckoutAndReviewBox} from "./CheckoutAndReviewBox";
 
 export const BookCheckoutPage = () => {
     const [book, setBook] = useState<BookModel>();
@@ -63,8 +65,10 @@ export const BookCheckoutPage = () => {
                             <h2>{book?.title}</h2>
                             <h5 className='text-primary'>{book?.author}</h5>
                             <p className='lead'>{book?.description}</p>
+                            <StarsReview rating={4} size={32}/>
                         </div>
                     </div>
+                    <CheckoutAndReviewBox book={book} mobile={false}/>
                 </div>
                 <hr/>
             </div>
@@ -80,8 +84,10 @@ export const BookCheckoutPage = () => {
                         <h2>{book?.title}</h2>
                         <h5 className='text-primary'>{book?.author}</h5>
                         <p className='lead'>{book?.description}</p>
+                        <StarsReview rating={4} size={32}/>
                     </div>
                 </div>
+                <CheckoutAndReviewBox book={book} mobile={true}/>
                 <hr/>
             </div>
         </div>
