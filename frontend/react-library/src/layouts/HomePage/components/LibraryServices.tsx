df232b5ee035017c6b11c803b76eaf228b8384e4
vakser <65089446+vakser@users.noreply.github.com>
2023-10-27 20:08:16 +0300

    Home page in React module of the app refactored with authentication.

--- frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
+++ frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
@@ -1,4 +1,8 @@
+import {useOktaAuth} from "@okta/okta-react";
+import {Link} from "react-router-dom";
+
 export const LibraryServices = () => {
+    const {authState} = useOktaAuth();
     return (
         <div className='container my-5'>
             <div className='row p-4 align-items-center border shadow-lg'>
@@ -6,7 +10,11 @@ export const LibraryServices = () => {
                     <h1 className='display-4 fw-bold'>Can't find what you are looking for?</h1>
                     <p className='lead'>If you cannot find what you are looking for, send our library's admin a personal message!</p>
                     <div className='d-grid gap-2 justify-content-md-start mb-4 mb-lg-3'>
-                        <a className='btn main-color btn-lg text-white' href='#'>Sign up</a>
+                        {authState?.isAuthenticated ?
+                            <Link to='#' type='button' className='btn main-color btn-lg px-4 me-md-2 fw-bold text-white'>Library Services</Link>
+                            :
+                            <Link className='btn main-color btn-lg text-white' to='/login'>Sign up</Link>
+                        }
                     </div>
                 </div>
                 <div className='col-lg-4 offset-lg-1 shadow-lg lost-image'></div>
