c040ae4049e830e3e555ed36102e47925ed6527b
vakser <65089446+vakser@users.noreply.github.com>
2023-10-27 17:18:51 +0300

    Configuration added to App.

--- frontend/react-library/src/lib/oktaConfig.ts
+++ frontend/react-library/src/lib/oktaConfig.ts
@@ -1,4 +1,4 @@
-export const octaConfig = {
+export const oktaConfig = {
     clientId: '0oacz17v97Wme1PV15d7',
     issuer: 'https://dev-98418519.okta.com/oauth2/default',
     redirectUri: 'http://localhost:3000/login/callback',
