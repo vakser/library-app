901721520d01cf91da6a7f6615761ad13fc5a9d7
vakser <65089446+vakser@users.noreply.github.com>
2023-10-28 14:00:42 +0300

    Security configuration added to Spring Boot module of the app.

---
+++ backend/spring-boot-library/src/main/java/com/example/springbootlibrary/config/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.example.springbootlibrary.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // disable Cross Site Request Forgery
+        http.csrf().disable();
+        // protect endpoints at /api/<type>/secure
+        http.authorizeRequests(configurer -> configurer
+                .antMatchers("/api/books/secure/**")
+                .authenticated())
+                .oauth2ResourceServer()
+                .jwt();
+        // add CORS filters
+        http.cors();
+        // add content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+        // force a non-empty response body for 401 to make the response friendly
+        Okta.configureResourceServer401ResponseBody(http);
+        return http.build();
+    }
+}
