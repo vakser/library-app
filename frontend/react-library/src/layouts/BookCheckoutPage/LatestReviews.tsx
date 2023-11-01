2587a7e9d5c2d402158890a11114458c59015a1e
vakser <65089446+vakser@users.noreply.github.com>
2023-11-01 12:47:07 +0200

    Review list (all reviews for a specific book) functionality implemented for the frontend module of the app.

--- frontend/react-library/src/layouts/BookCheckoutPage/LatestReviews.tsx
+++ frontend/react-library/src/layouts/BookCheckoutPage/LatestReviews.tsx
@@ -16,7 +16,7 @@ export const LatestReviews: React.FC<{ reviews: ReviewModel[], bookId: number | 
                             <Review review={eachReview} key={eachReview.id}></Review>
                         ))}
                         <div className='m-3'>
-                            <Link type='button' className='btn main-color btn-md text-white' to='#'>
+                            <Link type='button' className='btn main-color btn-md text-white' to={`/reviewlist/${props.bookId}`}>
                                 Reach all reviews
                             </Link>
                         </div>
