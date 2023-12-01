98b6fe54424f07ecebd31c6805eefdb5a86a4ab0
vakser <65089446+vakser@users.noreply.github.com>
2023-12-01 09:14:05 +0200

    History model created in frontend module of the app.

--- backend/spring-boot-library/src/main/java/com/example/springbootlibrary/entity/History.java
+++ backend/spring-boot-library/src/main/java/com/example/springbootlibrary/entity/History.java
@@ -25,18 +25,18 @@ public class History {
     @Column(name = "description")
     private String description;
     @Column(name = "image")
-    private String image;
+    private String img;
 
     public History() {}
 
     public History(String userEmail, String checkoutDate, String returnedDate, String title, String author,
-                   String description, String image) {
+                   String description, String img) {
         this.userEmail = userEmail;
         this.checkoutDate = checkoutDate;
         this.returnedDate = returnedDate;
         this.title = title;
         this.author = author;
         this.description = description;
-        this.image = image;
+        this.img = img;
     }
 }
