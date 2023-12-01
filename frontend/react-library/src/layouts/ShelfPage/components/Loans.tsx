068eeea9db3de5fc77088d315c4527eeabced8f6
vakser <65089446+vakser@users.noreply.github.com>
2023-12-01 08:19:55 +0200

    Renew book loan functionality implemented in frontend module and History entity created in backend module of the app.

--- frontend/react-library/src/layouts/ShelfPage/components/Loans.tsx
+++ frontend/react-library/src/layouts/ShelfPage/components/Loans.tsx
@@ -68,6 +68,21 @@ export const Loans = () => {
         }
         setCheckout(!checkout);
     }
+    async function renewLoan(bookId: number) {
+        const url = `http://localhost:8080/api/books/secure/renew/loan/?bookId=${bookId}`;
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        };
+        const returnResponse = await fetch(url, requestOptions);
+        if (!returnResponse.ok) {
+            throw new Error('Something went wrong!');
+        }
+        setCheckout(!checkout);
+    }
     return (
         <div>
             {/* Desktop */}
@@ -127,7 +142,7 @@ export const Loans = () => {
                                     </div>
                                 </div>
                                 <hr/>
-                                <LoansModal shelfCurrentLoan={shelfCurrentLoan} mobile={false} returnBook={returnBook}/>
+                                <LoansModal shelfCurrentLoan={shelfCurrentLoan} mobile={false} returnBook={returnBook} renewLoan={renewLoan}/>
                             </div>
                         ))}
                     </> :
@@ -196,7 +211,7 @@ export const Loans = () => {
                                         </Link>
                                     </div>
                                 </div>
-                                <LoansModal shelfCurrentLoan={shelfCurrentLoan} mobile={true} returnBook={returnBook}/>
+                                <LoansModal shelfCurrentLoan={shelfCurrentLoan} mobile={true} returnBook={returnBook} renewLoan={renewLoan}/>
                                 <hr/>
                             </div>
                         ))}
--- frontend/react-library/src/layouts/ShelfPage/components/LoansModal.tsx
+++ frontend/react-library/src/layouts/ShelfPage/components/LoansModal.tsx
@@ -1,7 +1,7 @@
 import ShelfCurrentLoans from "../../../models/ShelfCurrentLoans";
 import React from "react";
 
-export const LoansModal: React.FC<{ shelfCurrentLoan: ShelfCurrentLoans, mobile: boolean, returnBook: any }> = (props) => {
+export const LoansModal: React.FC<{ shelfCurrentLoan: ShelfCurrentLoans, mobile: boolean, returnBook: any, renewLoan: any }> = (props) => {
     return (
         <div className='modal fade' id={props.mobile ? `mobilemodal${props.shelfCurrentLoan.book.id}` :
             `modal${props.shelfCurrentLoan.book.id}`} data-bs-backdrop='static' data-bs-keyboard='false'
@@ -52,7 +52,10 @@ export const LoansModal: React.FC<{ shelfCurrentLoan: ShelfCurrentLoans, mobile:
                                             aria-current='true'>
                                         Return Book
                                     </button>
-                                    <button data-bs-dismiss='modal'
+                                    <button onClick={props.shelfCurrentLoan.daysLeft < 0 ?
+                                            (event) => event.preventDefault() :
+                                            () => props.renewLoan(props.shelfCurrentLoan.book.id)
+                                        } data-bs-dismiss='modal'
                                             className={
                                                 props.shelfCurrentLoan.daysLeft < 0 ?
                                                     'list-group-item list-group-item-action inactiveLink' :
