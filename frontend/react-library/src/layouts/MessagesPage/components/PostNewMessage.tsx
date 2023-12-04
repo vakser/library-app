ae663c70046e42ae837ff2a4ace10e04804fae9e
vakser <65089446+vakser@users.noreply.github.com>
2023-12-04 08:54:22 +0200

    PostNewMessage component functionality implemented in React module of the app.

--- frontend/react-library/src/layouts/MessagesPage/components/PostNewMessage.tsx
+++ frontend/react-library/src/layouts/MessagesPage/components/PostNewMessage.tsx
@@ -1,5 +1,6 @@
 import { useOktaAuth } from '@okta/okta-react';
 import { useState } from 'react';
+import MessageModel from "../../../models/MessageModel";
 
 export const PostNewMessage = () => {
     const { authState } = useOktaAuth();
@@ -7,13 +8,38 @@ export const PostNewMessage = () => {
     const [question, setQuestion] = useState('');
     const [displayWarning, setDisplayWarning] = useState(false);
     const [displaySuccess, setDisplaySuccess] = useState(false);
+    async function submitNewQuestion() {
+        const url = `http://localhost:8080/api/messages/secure/add/message`;
+        if (authState?.isAuthenticated && title !== '' && question !== '') {
+            const messageRequestModel: MessageModel = new MessageModel(title, question);
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(messageRequestModel)
+            };
+            const submitNewQuestionResponse = await fetch(url, requestOptions);
+            if (!submitNewQuestionResponse.ok) {
+                throw new Error('Something went wrong!');
+            }
+            setTitle('');
+            setQuestion('');
+            setDisplayWarning(false);
+            setDisplaySuccess(true);
+        } else {
+            setDisplayWarning(true);
+            setDisplaySuccess(false);
+        }
+    }
     return (
         <div className='card mt-3'>
-            {displaySuccess && <div className='alert alert-success' role='alert'>Question added successfully</div>}
             <div className='card-header'>Ask question to Luv2Read Admin</div>
             <div className='card-body'>
                 <form method='POST'>
                     {displayWarning && <div className='alert alert-danger' role='alert'>All fields must be filled out</div>}
+                    {displaySuccess && <div className='alert alert-success' role='alert'>Question added successfully</div>}
                     <div className='mb-3'>
                         <label className='form-label'>Title</label>
                         <input type='text' className='form-control' id='exampleFormControlInput1' placeholder='Title'
@@ -26,7 +52,7 @@ export const PostNewMessage = () => {
                         </textarea>
                     </div>
                     <div>
-                        <button type='button' className='btn btn-primary mt-3'>Submit Question</button>
+                        <button type='button' className='btn btn-primary mt-3' onClick={submitNewQuestion}>Submit Question</button>
                     </div>
                 </form>
             </div>
