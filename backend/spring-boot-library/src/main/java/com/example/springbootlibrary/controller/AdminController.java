a6c62f443fcc867ab91605d0717671e4e4360a65
vakser <65089446+vakser@users.noreply.github.com>
2023-12-18 20:57:55 +0200

    Functionality for increasing/decreasing quantity of book by admin added to backend of the app.

--- backend/spring-boot-library/src/main/java/com/example/springbootlibrary/controller/AdminController.java
+++ backend/spring-boot-library/src/main/java/com/example/springbootlibrary/controller/AdminController.java
@@ -15,6 +15,24 @@ public class AdminController {
         this.adminService = adminService;
     }
 
+    @PutMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only!");
+        }
+        adminService.increaseBookQuantity(bookId);
+    }
+
+    @PutMapping("/secure/decrease/book/quantity")
+    public void decreaseBookQuantity(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only!");
+        }
+        adminService.decreaseBookQuantity(bookId);
+    }
+
     @PostMapping("/secure/add/book")
     public void postBook(@RequestHeader(value = "Authorization") String token, @RequestBody AddBookRequest addBookRequest) throws Exception {
         String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
--- backend/spring-boot-library/src/main/java/com/example/springbootlibrary/service/AdminService.java
+++ backend/spring-boot-library/src/main/java/com/example/springbootlibrary/service/AdminService.java
@@ -6,6 +6,7 @@ import com.example.springbootlibrary.requestmodels.AddBookRequest;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -14,6 +15,26 @@ public class AdminService {
 
     public AdminService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public void increaseBookQuantity(Long bookId) throws Exception {
+        Optional<Book> book = bookRepository.findById(bookId);
+        if (book.isEmpty()) {
+            throw new Exception("Book not found");
+        }
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
+        book.get().setCopies(book.get().getCopies() + 1);
+        bookRepository.save(book.get());
+    }
+
+    public void decreaseBookQuantity(Long bookId) throws Exception {
+        Optional<Book> book = bookRepository.findById(bookId);
+        if (book.isEmpty() || book.get().getCopiesAvailable() <= 0 || book.get().getCopies() <= 0) {
+            throw new Exception("Book not found or quantity locked");
+        }
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() - 1);
+        book.get().setCopies(book.get().getCopies() - 1);
+        bookRepository.save(book.get());
     }
 
     public void postBook(AddBookRequest addBookRequest) {
