0ff7220bc422b34b0c9b18e392ba08d4f8982e08
vakser <65089446+vakser@users.noreply.github.com>
2023-12-21 08:47:47 +0200

    React - Okta HTTPS config updated.

--- frontend/react-library/src/lib/oktaConfig.ts
+++ frontend/react-library/src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oacz17v97Wme1PV15d7',
     issuer: 'https://dev-98418519.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true
